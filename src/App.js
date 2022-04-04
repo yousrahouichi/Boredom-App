42878fe619c146e91e9aee4f357b5b5633963ad8
yousrahouichi <yousra.houichi@imadrassa.com>
2022-04-04 13:55:48 +0100

    get random activity

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+import Activity from "./components/Activity";
 import Navigation from "./components/Navigation";
 
 function App() {
+  const [activity, setActivity] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "http://www.boredapi.com/api/activity/"
+        );
+        setActivity(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.error(error.message);
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
     <div className="App">
       <Navigation />
+      <Activity
+        title={activity?.activity}
+        accessibility={activity?.accessibility}
+        type={activity?.type}
+        participants={activity?.participants}
+        price={activity?.price}
+        link={activity?.link}
+      />
     </div>
   );
 }
---
+++ src/components/Activity.js
@@ -0,0 +1,29 @@
+import { Card, ListGroup, ListGroupItem } from "react-bootstrap";
+const Activity = ({
+  title,
+  accessibility,
+  type,
+  participants,
+  price,
+  link,
+}) => {
+  return (
+    <Card style={{ width: "18rem" }}>
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+      </Card.Body>
+      <ListGroup className="list-group-flush">
+        <ListGroupItem>Accessibility: {accessibility}</ListGroupItem>
+        <ListGroupItem>Type: {type}</ListGroupItem>
+        <ListGroupItem>Participants: {participants}</ListGroupItem>
+        <ListGroupItem>Price: {price}</ListGroupItem>
+        {link && <ListGroupItem>Link: {link}</ListGroupItem>}
+      </ListGroup>
+      <Card.Body>
+        <Card.Link href="#">Add to favorites</Card.Link>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default Activity;
